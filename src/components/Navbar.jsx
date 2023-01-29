1122d3c77c3b667cf7aa71a4d956e35074eb2b42
Nester Okertchiri <naokertchiri@gmail.com>
2023-01-29 13:47:08 +0000

    seed init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div className="text-white">
+      <h1>Axon</h1>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
